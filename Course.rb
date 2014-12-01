6658f2e2ed47c67ee6a2f877350dbc6f856df6ba
aemery <agemery2@gmail.com>
2014-12-01 04:58:39 -0500

    will probably need to reorganize file/class structure at somepoint. works for now

--- Course.rb
+++ Course.rb
@@ -5,25 +5,20 @@ class Course
 #course is the correct one when adding them.
 
 	attr_reader :code, :name, :meet_times
-	#meet_times would only be used when you want
-	#a SPECIFIC course time. meet_times shall be stored as a String
+	#meet_times is an array of Strings representing the acceptable
+	#meeting times for the course
 	#in the same format as seen in Firefox by Selenium.
 	#alternatively ^, we could use section # instead....
 
 	def initialize(course_code, course_name="", course_meet_times="")
 		@code = course_code
 		@name = course_name
-		@meet_times = meet_times
+		@meet_times = course_meet_times
 	end 
 
-	def get_column_index_to_check()
-		if (!meet_times.eql? "")
-			return 3 #not sure if this is the right column change it later
-		elsif (!name.eql? "")
-			return 2 #again not sure right now but w/e
-		else
-			return 0
-		end
+	def check_match(row)
+		#need to figure something out as far as checking the specific course
+		return row.find_elements(:tag_name, "td")[0].text.include? @code
 	end
 			
 end 
--- main_course.rb
+++ main_course.rb
@@ -3,66 +3,84 @@
 
 #this file holds the main function of the course-grabbing program.
 
+require './Course.rb'
+require 'selenium-webdriver'
+
+
 def main_course(username, password) 
 	#pls pass the parameters
 	#maybe I will add a prompt in case they are null
 
 	#this might change to be a parameter idk
-	courses = ["EEL4744", "EES"]
+	courses = [(Course.new("EEL474")), (Course.new("CIS4930"))]
 
 	#selenium web driver
 	driver = Selenium::WebDriver.for(:firefox) 
 	#this is the website the program is designed for
-	driver.get "http://isis.ufl.edu" 
+	driver.get "http://isis.ufl.edu"
+	valid_courses =[] 
 
 	log_in(driver, username, password)
 	courses.each do |c|
-		course_search(driver, c)
+		valid_courses << get_courses(driver, c)
 	end
+
+	puts valid_courses
 end
 
 def log_in(driver, username, password)
 
 	begin #open up the pull down menu in case we need to
-		SpringButton = driver.find_element(:link, "- Spring")
-		SpringButton.click
+		springButton = driver.find_element(:link, "- Spring")
+		springButton.click
 
 	rescue Selenium::WebDriver::Error::NoSuchElementError
-		RegistrationButton = driver.find_element(:link, "Registration")
-		RegistrationButton.click
+		registrationButton = driver.find_element(:link, "Registration")
+		registrationButton.click
 
-		SpringButton = driver.find_element(:link, "- Spring")
-		SpringButton.click
+		springButton = driver.find_element(:link, "- Spring")
+		springButton.click
 	end
 
 	#now for actually logging in...
-	UsernameField = driver.find_element(:id, "username")
-	UsernameField.send_keys(username)
+	usernameField = driver.find_element(:id, "username")
+	usernameField.send_keys(username)
 
-	PasswordField = driver.find_element(:id, "password")
-	PasswordField.send_keys(password)
+	passwordField = driver.find_element(:id, "password")
+	passwordField.send_keys(password)
 
-	LoginButton = driver.find_element(:name, "login")
-	LoginButton.click
+	loginButton = driver.find_element(:name, "login")
+	loginButton.click
 end 
 
-def course_search(driver, course)
-	CourseSearch = driver.find_element(:link, "Search All Courses");
-	CourseSearch.click;
+def get_courses(driver, course)
+	courseSearch = driver.find_element(:link, "Search All Courses");
+	courseSearch.click;
 
-	CourseCodeField = driver.find_element(:name, "REGCSE"); #thats the name of the input field...
-	CourseCodeField.send_keys(course.code);
+	courseCodeField = driver.find_element(:name, "REGCSE"); #thats the name of the input field...
+	courseCodeField.send_keys(course.code);
 
-	SearchButton = driver.find_element(:xpath, "//input[@value='Search']"); #find the search button
-	SearchButton.click;
+	searchButton = driver.find_element(:xpath, "//input[@value='Search']"); #find the search button
+	searchButton.click;
 
 	#the resultant table in the following page
-	Table = driver.find_elements(:xpath, "//table")[0]; #first table in the page is the one we want
-	TableRows = Table.find_elements(:tag_name, "tr"); #get the rows
-	Courses = [] #initialize array for the rows matching the desired course
-	column_index = course.get_column_index_to_check #the index of the column containing the 
-	#most specific information; e.g. if course meet times is specified, check that column
+	table = driver.find_elements(:xpath, "//table")[0]; #first table in the page is the one we want
+	tableRows = table.find_elements(:tag_name, "tr"); #get the rows
+	acceptable_courses = [] #initialize array for the rows matching the desired course
+	
 
-	TableRows[1..-1].each do |row|
-		if (row.find_elements)
+	tableRows[1..-1].each do |row| #the 0th row is a header row and we do not want it
+		if (course.check_match(row))
+			acceptable_courses << row 
+			#do we want an array of acceptable courses, or do we want to just go ahead and select the course when it comes up?
+		end
+	end 
+	return acceptable_courses
+end 
 
+puts "Enter the username"
+username = gets.chomp
+puts "Enter the password lol"
+password = gets.chomp
+
+main_course(username, password)
